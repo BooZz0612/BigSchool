2f0ffa08da5b49ca2ecf55a2daf711cc7d87cae2
haji0 <haji0@DESKTOP-SGRTQCD>
2023-05-25 23:18:05 +0700

    Add project files.

---
+++ BigSchool/Models/Course.cs
@@ -0,0 +1,25 @@
+﻿using BigSchool.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BigSchool.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public ApplicationUser Lecture { get; set; }
+        [Required]
+        public string LectureID { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Place { get; set; }
+        public DateTime Datetime { get; set; }
+        public byte Category { get; set; }
+
+        [Required]
+        public string CategoryID { get; set; }
+    }
+}
